a4c2c7c000417ae18edaa4c9a2beea42d7de5e2d
Tharan Raj <tharanseriously@gmail.com>
2016-06-05 09:44:26 +0800

    fixing callback

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  #before_action :authenticate_user!
-  #before_action :user_preferences_present?, except: [:authenticate_user!]
+  before_action :authenticate_user!
+  before_action :user_preferences_present?, except: [:authenticate_user!]
 
   def user_preferences_present?
-    if current_user.preferences.first.tags == []
-      redirect_to user_preferences_home_index_path
+    if current_user.present?
+      if current_user.preferences == nil
+        redirect_to user_preferences_home_index_path
+      end
     end
   end
 end
